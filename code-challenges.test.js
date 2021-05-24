0e4e2610f0735910f95ff0bd4dfd639c4ca6a85c
hexflo <hexflores87@yahoo.com>
2021-05-24 08:27:37 -0700

    completed week 3 assessment

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -18,6 +18,31 @@
 
 // a) Create a test with expect statements for each of the variables provided.
 
+// The first step I want to do is create a function. 
+// The first step to creating a function is to declare a variable- example const.
+// Second step is to create a function name which allows us to call on the function.
+
+const numsGreat = (num) => { // call numsGreat with value greater than 1
+    if (num<2)
+        return num;
+    let arr = Array(num); // create an empty with a size of number passed in
+    arr[0]=1; // set first element to 1.
+    arr[1]=1; // set second element to 1.
+
+    // for 2 to num less 1 do the following
+    for (i=2;i<num;i++)
+    {
+        // current array element is equal the sum of the prior two
+        arr[i]=arr[i-1]+arr[i-2];
+    }
+    return arr; // return array
+}
+let arr2 = numsGreat(2);
+console.log(arr2);
+let arr6 = numsGreat(6);
+console.log(arr6);
+let arr10 = numsGreat(10);
+console.log(arr10);
 // Example input: 6
 // Expected output: [1, 1, 2, 3, 5, 8]
 
@@ -29,21 +54,97 @@
 
 
 // b) Create the function that makes the test pass.
+/*
+fdescribe("sortedOddNums", () =>{
 
+    it("returns only sorted odd numbers", () =>{
+        expect(sortedOddNums( fullArr1)).toEqual([-9, 7, 9, 199])
+        expect(sortedOddNums( fullArr2)).toEqual([-823, 7, 23])
+    })
+})
 
+*/
 
+// 
 
 
 // --------------------2) Create a function that takes in an array and returns a new array of only odd numbers sorted from least to greatest.
 
 // a) Create a test with expect statements for each of the variables provided.
 
-var fullArr1 = [4, 9, 0, "7", 8, true, "hey", 7, 199, -9, false, "hola"]
+// declare a var of a test array 
+//declare a function called onlyOdd
+// takes in an array as an argument
+// Returns a NEW array with all odd numbers sorted from least to greatest.
+
+// call and test function using Jest. 
+
+var testArr = [4, 9, 0, "7", 8, true, "hey", 7, 199, -9, false, "hola"]; //declare a var of test array
+//expected output = [-9, 7, 9, 199] 
+function sortedOddNums(arr) // call function sortedOddNums with array
+{
+    // create and initialize new array for sorted odd numbers to return.
+    let retArr=[];
+    // for each number in the passed in array:
+    for (let i in arr)
+    {
+        let val = arr[i]; // get value at index in array
+        // if is an integer:
+        if (Number.isInteger(val)) 
+        {
+            val = parseInt(val); // convert value to integer
+            // if odd number:
+            if (val % 2 != 0) 
+            {
+                console.log("push: "+val);
+                // add it to the new array to return.
+                retArr.push(parseInt(val));
+            }
+        }
+    }
+    // sort and return new array.
+    return retArr.sort((a,b)=>{return a-b;});;
+}
+let values = sortedOddNums(testArr);
+console.log(values);
+
+
+
+
+/*
+const onlyOdd = (arr) => {
+    var newArr = []
+    for(let i=0; i< arr.length; i++){
+        if(arr[i] % 2 != 0){
+            newArr.push(arr[i])
+        }
+    }
+    return newArr
+}
+
+
+
+//var fullArr1 = [4, 9, 0, "7", 8, true, "hey", 7, 199, -9, false, "hola"]
 // Expected output: [-9, 7, 9, 199]
 
 var fullArr2 = ["hello", 7, 23, -823, false, 78, null, "67", 6, "number"]
 // Expected output: [-823, 7, 23]
 
+var testArr2 =  ["hello", 7, 23, -823, false, 78, null, "67", 6, "number"]
+// Expected output: [-823, 7, 23]
+   
+function onlyOdd2(arr){
+    var newArr = [];
+    for(let i=0; i<arr.length; i++){
+        if(arr[i] % 2 != 0){
+            newArr.push(arr[i])
+        }
+    }
+    return newArr
+}
+console.log(onlyOdd(testArr));
+
+*/
 
 
 
@@ -56,14 +157,33 @@ var fullArr2 = ["hello", 7, 23, -823, false, 78, null, "67", 6, "number"]
 // --------------------3) Create a function that takes in an array and returns an array of the accumulating sum. An empty array should return an empty array.
 
 // a) Create a test with expect statements for each of the variables provided.
-
-var numbersToAdd1 = [2, 4, 45, 9]
+function accumArraySum (arr) // call accumArraySum with array
+{
+    if (arr.length<1) // if array is empty return empty array
+        return arr;
+    let retArr = Array(arr.length); // create return array of same size as passed in array
+    let sum = 0; // create and initialize sum to 0.
+    // for each number in passed in array:
+    for (let i in arr)
+    {
+        sum += arr[i]; // sum each number
+        retArr[i]=sum; // add sum to same index element in return array.
+    }
+    return retArr; // return new array.
+}
+var numbersToAdd1 = [2, 4, 45, 9];
+let sumArr=accumArraySum(numbersToAdd1);
+console.log(sumArr);
 // Excpected output: [2, 6, 51, 60]
 
 var numbersToAdd2 = [0, 7, -8, 12]
+sumArr=accumArraySum(numbersToAdd2);
+console.log(sumArr);
 // Expected output: [0, 7, -1, 11]
 
 var numbersToAdd3 = []
+sumArr=accumArraySum(numbersToAdd3);
+console.log(sumArr);
 // Expected output: []
 
 
--- interview-questions.md
+++ interview-questions.md
@@ -17,60 +17,64 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 2. What is the difference between a HTML/JSX div and a span?
 
-  Your answer:
+  Your answer: HTML/JSX is what you see on the DOM. Span is what is being rendered on the React.
 
-  Researched answer:
+  Researched answer: Span is used for small chunk and div uses a large chunk of the HTML codes. The div tag is most essentially used to HTML IN WEB DEVELOPMENT BECAUSE IT HELPS TO SEPERATE  out data in the web pages and add only particular dta to the web pages.
 
 
 
 3. What is a ternary operator in JavaScript?
 
-  Your answer:
+  Your answer: I remember when I was working with Chris and Shaz they kept mentioning the ternary operator. I know it was a big issue on solving the fundamnetal aspect of the challenge. 
 
-  Researched answer:
+  Researched answer: The ternary operator in Javascript is the only JavaScript operator that takes three operands: a condition followed by a question mark (?), then an expression to execute if the conidtion is truthy followed by a colon (:), and finally the expression to execute if the condition os falsy. 
 
 
 
 4. What are props in React?
 
-  Your answer:
+  Your answer: Props in react (props are short for properties) 
 
-  Researched answer:
+  Researched answer: “Props” is a special keyword in React, which stands for properties and is being used for passing data from one component to another. But the important part here is that data with props are being passed in a uni-directional flow. ( one way from parent to child)
 
 
 
 5. What is `this` in JavaScript?
 
-  Your answer:
+  Your answer: I understand 'this' is being used to call upon an object inside a function.
 
-  Researched answer:
+  Researched answer: “this” Refers to an Invoker Object (Parent Object)
+
+In JavaScript, the property of an object can be a method or a simple value. When an object's method is invoked, then this refers to the object which contains the method being invoked.
 
 
 
 6. STRETCH: What is an anonymous function in JavaScript?
 
-  Your answer:
+  Your answer: I think this is when a function is being created under the parent class component and before the render.
 
-  Researched answer:
+  Researched answer: An anonymous function is a function without a name. An anonymous function is often not accessible after its initial creation. ... Because we need to call the anonymous function later, we assign the function to the show variable.
 
 
 
 7. STRETCH: What does it mean to create a responsive UI? What are three options for creating responsive design?
 
-  Your answer:
+  Your answer:  I know UI stands for user-interface. I know the user-interface is the where the person controls a software device. I use the remote control as a good example to always remember this defintion. 
 
-  Researched answer:
+  Researched answer: Fluid grids: Fluid grids are based upon the sizing of page elements as opposed to using pixels. ...
+Media queries: Media queries allow the gathering of information about a site visitor. ...
+Flexible images: The size of flexible images is dictated by relative units so they are kept inside of their element.
 
 
 
 ## Looking Ahead: Terms for Next Week
 
-1. Ruby:
+1. Ruby: Ruby is a programming language created in 1993. It is an open source object-oritented langauge with a focus on simplicity.
 
-2. Object oriented programming:
+2. Object oriented programming: OOP is a programming paradigm based on the concept of "objects", which can contain data and code. 
 
-3. RSpec:
+3. RSpec: RSpec is a testing tool for Ruby, created for behavior-driven development (BDD). It is the most frequently used testing library for Ruby in production applications
 
-4. Ruby blocks:
+4. Ruby blocks: Ruby blocks are anonymous functions that can be passed into methods. Blocks are enclosed in a do-end statement or curly braces {}. ... We can give the caller of this method the total flexibility of passing in some code, not just the argument age, and the responsibility of our method would be to execute the code.
 
-5. Ruby hashes:
+5. Ruby hashes:A Hash is a collection of key-value pairs like this: "employee" = > "salary". It is similar to an Array, except that indexing is done via arbitrary keys of any object type, not an integer index.
